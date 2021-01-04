aaea1c906c55ae0cdc2373c566dbdfc92fd8018b
Benikasas <justinasbenikasas@gmail.com>
2021-01-04 16:37:33 +0000

    Worked on the disturbinator for loop. Still need to fix it

--- Relative_tools.py
+++ Relative_tools.py
@@ -16,21 +16,20 @@ def Delta_gen(epsilon):
     return Delta
 
 ## A function to collect a large number of Deltas
-def fluctuations(Nfluc,epsilon):
-    disturbance=[]
+def fluctuations(Nfluc=1000, epsilon=0.2):
+    disturbances=[]
     for i in range(Nfluc):
         X=Delta_gen(epsilon)
-        disturbance.append(X)
-    return disturbance
+        disturbances.append(X)
+    return disturbances
 
 class spacetime():
     def __init__(self, Nt, Nx, Ny, Nz, beta, u0, U=None):
         if None == U:
             # Creates a spacetime grid, and at every point theres 4 
-            U = [[[[np.zeros(4) for z in range(Nz)] for y in range(Ny)] for x in range(Nx)] for t in range(Nt)]
+            U = [[[[[np.zeros(1) for mu in range(4)] for z in range(Nz)] for y in range(Ny)] for x in range(Nx)] for t in range(Nt)]
         # convert to numpy arrays -> significant speed up
         self.U = np.array(U)
-        
         ## Unsure about the necesity of this
         # self.beta = beta
         # self.u0 = u0
@@ -41,18 +40,23 @@ class spacetime():
     
     ## A function to disturb the initialized empty spacetime array by adding random numbers to U via fluctuations fn.
     ## Decided to put it as a separate function for cleanliness
-    def distrubinator()
+    def disturbinator(self, disturbances, U):
+        U_prime=U
         for t in range(Nt):
-            for x in range(Nt):
+            for x in range(Nx):
                 for y in range(Ny):
                     for z in range(Nz):
-                        i=random.
-                        U_prime=U(Nt, Nx, Ny, Nz)
+                        r=np.random.randint(0, Nfluc)
+                        for mu in range(4):
+                            disturbance=disturbances[r]
+                            U_prime[t, x, y, z, mu]=self.U[t, x, y, z, mu]+disturbance[mu]
+        U=U_prime
+        return U
 
 
     ## First order approximation in a direction
     ## Make a for loop in the future
-    def first_approx():
+    def first_approx_tool(self):
         # directions=[Nt, Nx, Ny, Nz]
         # for i in directions:
         #     U_og=trial.U(Nt, Nx, Ny, Nz)
@@ -60,18 +64,25 @@ class spacetime():
         #     U_prime_t=trial.U(Nt, Nx, Ny, Nz)       ## Might actually add to the value but might not move forward in direction
         #     partial_approx_t=(U_prime_t-U_og)/lattice_space
         #     approx+=partial_approx_t
-        U_og=trial.U(Nt, Nx, Ny, Nz)
-        U_t=trial.U(Nt+1, Nx, Ny, Nz)
-        U_x=trial.U(Nt, Nx+1, Ny, Nz)
-        U_y=trial.U(Nt, Nx, Ny+1, Nz)
-        U_z=trial.U(Nt, Nx, Ny, Nz+1)        
+        U_og=self.U(Nt, Nx, Ny, Nz)
+        U_t=self.U(Nt+1, Nx, Ny, Nz)
+        U_x=self.U(Nt, Nx+1, Ny, Nz)
+        U_y=self.U(Nt, Nx, Ny+1, Nz)
+        U_z=self.U(Nt, Nx, Ny, Nz+1)        
         diff_t=(U_t-U_og)/lattice_space
         diff_x=(U_x-U_og)/lattice_space
         diff_y=(U_y-U_og)/lattice_space
         diff_z=(U_z-U_og)/lattice_space
         total_diff=diff_t+diff_x+diff_y+diff_z
         return total_diff
+    
+def generator():
+    U=spacetime(Nt, Nx, Ny, Nz, beta, u0)
+    disturbances=fluctuations()
+    U_prime=U.disturbinator(disturbances, U)
+    first_approx=first_approx_tool()
 
-value=fluctuations(Nfluc, epsilon)
+value=generator()
 
-print()
+
+
