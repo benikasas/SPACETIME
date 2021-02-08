8f3b4d3b3e8a1784d0312e8be3accb3af82067af
Benikasas <justinasbenikasas@gmail.com>
2021-02-08 14:10:57 +0000

    Generate.py is with semi-decent comments.

--- generate.py
+++ generate.py
@@ -10,42 +10,28 @@ import functools
 import datetime
 import gauge_latticeqcd
 
-### settings
-# Nt = 8
-# Nx = 8
-# Ny = 8
-# Nz = 8
-# Ncfg = 1000          # number of lattices to generate
+### settings  
 # action = 'W'    # W = Wilson, Wilson with rectangle improvements, W_T and WR_T = With tadpole improvement
-# betas = [5.5]      # betas to be generated, beta = 6/g^2
-# startcfg = 900     # warm start (0) or existing cfg number to start the Markov chain
-# Nhits = 50         # hits between each update
-# Nmatrix = 10000    # number of random SU(3) matrices to be used for updates
-# epsilon = 0.24      # how "far" away from identity the updates will be
-# threads = 4       # threads used in multiprocessing
-
 Nt = 16
 Nx = 16
 Ny = 16
 Nz = 16
-Ncfg = 1000          
-action = 'W'    
-betas = [5.7]      
-startcfg = 102      
-Nhits = 50         
-Nmatrix = 10000   
-epsilon = 0.20    
-threads = 4 
+Ncfg = 1000         # number of lattices to generate
+action = 'W'        ########## Spacetime part implemented only for Wilson action    
+betas = [5.7]       # betas to be generated, beta = 6/g^2
+startcfg = 102      # warm start (0) or existing cfg number to start the Markov chain
+Nhits = 50          # hits between each update
+Nmatrix = 10000     # Number of matrices to generate for LQCD
+epsilon = 0.20      # how "far" away from identity the updates will be
+threads = 4         # threads used in multiprocessing
 
-Nu0_step = 1       # if tadpole improving, number of cfgs to skip between calculating u0.
-Nu0_avg = 1        # if tadpole improving, number of u0 values to average together before updating
-u0 = 1.            # u0 = <W11>^(1/4); if tadpole improving and continuing from existing lattices, set here.  Else ignore.
+Nu0_step = 1        # if tadpole improving, number of cfgs to skip between calculating u0.
+Nu0_avg = 1         # if tadpole improving, number of u0 values to average together before updating
+u0 = 1.             # u0 = <W11>^(1/4); if tadpole improving and continuing from existing lattices, set here.  Else ignore.
 thermal=100         # Number of configurations before starting the general relativity part
-border=3            # The border of spacetime lattice that will equal to 0
+border=4            # Defines edges over which there will be no spacetime deformations, and the action will come from LQCD only
 
 # ### initialize multiprocessing
-
-
 if __name__ == '__main__':
     ### generate lattices
     for b in betas:
@@ -57,7 +43,7 @@ if __name__ == '__main__':
             os.mkdir(dir_name)
         else:
             print("Directory exists for beta ", b)
-        ### Same for spacetime deformations
+        ### Same for spacetime deformations and Ricci
         if not os.path.exists(dir_name_1):
             os.mkdir(dir_name_1)
         else:
@@ -71,9 +57,9 @@ if __name__ == '__main__':
     # Acceptance_result=gauge_latticeqcd.generate(beta=betas[0], u0=u0, action=action, Nt=Nt, Nx=Nx, Ny=Ny, Nz=Nz, startcfg=startcfg, Ncfg=Ncfg, thermal=thermal, border=border, Nhits=Nhits, Nmatrix=Nmatrix, epsilon=epsilon, Nu0_step=Nu0_step, Nu0_avg=Nu0_avg)
 
 
-    
+    #### Reinstalled multiprocessing in hopes that it actually does something
     p = Pool(threads)
-    # ### function to be calculated needs to use functools to work with map
+    ### function to be calculated needs to use functools to work with map
     func = functools.partial(gauge_latticeqcd.generate, u0=u0, action=action, Nt=Nt, Nx=Nx, Ny=Ny, Nz=Nz, startcfg=startcfg, Ncfg=Ncfg, thermal=thermal, border=border, Nhits=Nhits, Nmatrix=Nmatrix, epsilon=epsilon, Nu0_step=Nu0_step, Nu0_avg=Nu0_avg)
     p.map(func, betas) # call multiprocessing map function
     p.terminate()      # terminate multiprocessing
